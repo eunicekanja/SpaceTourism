61d00722fd898636aa287ab51ba145fc84a23954
Eunice Kanja <kanjawambui30@gmail.com>
2022-04-15 13:57:24 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DestinationComponent } from './destination/destination.component';
+import { CrewComponent } from './crew/crew.component';
+import { TechComponent } from './tech/tech.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DestinationComponent,
+    CrewComponent,
+    TechComponent
   ],
   imports: [
     BrowserModule,
